97855237cede15a2da52aeebd0cad402497a629a
Tom Ekshtein <tekshteint@gmail.com>
2023-09-06 19:59:57 -0700

    Working Insights

---
+++ spring-modulith-example-full/src/main/java/example/controllers/InventoryController.java
@@ -0,0 +1,34 @@
+package example.controllers;
+
+import example.order.Order;
+import example.order.OrderManagement;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Tom Ekshtein
+ */
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    private final OrderManagement orderManagement;
+
+    public InventoryController(OrderManagement orderManagement) {
+        this.orderManagement = orderManagement;
+    }
+
+    @GetMapping("/create")
+    @WithSpan
+    public String createOrder() {
+        Order order = new Order();
+
+        orderManagement.complete(order);
+
+        return "Order created with ID: " + order.getId();
+    }
+}
